b1f052371d7bdaaf3be1c096e73146349346eeec
000Name <i0685331@gmail.com>
2025-09-26 01:17:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,56 @@
-posindex = {"SongName": 0 , "ArtistName": 1, "Genre": 2,"Length": 3} #const,store length in seconds
+class Musiclibrary:
+    posindex = {"SongName": 0, "ArtistName": 1, "Genre": 2, "Length": 3}  #constant stored, length is stored in seconds
 
+    def __init__(self, file): #allows easier object creation, file is usable within the 
+        self.file = file
+        self.musicdata = self.load_data() #originally within the loaddata function,changed to here so code doesnt have to call load data everytime 
 
-def data(library):
-    with open(library,"r") as d:
-        values = d.readline()
-        musicdata = [value.strip().split(",") for value in values] #format of the txt would be song,artist,genre,length ,.split by , gives 4 seperate values from one line
-    return musicdata
+    def load_data(self):
+        with open(self.filepath, "r") as d:
+            values = d.readlines()
+            return [value.strip().split(",") for value in values] 
+            #format of the txt would be song,artist,genre,length ,.split by , gives 4 seperate values from one line
 
-
-def sort(musicdata, posname, reverse=False):
-    return sorted(musicdata,key=lambda x: int(x[posindex[posname]]) if posname == "Length" else x[posindex[posname]],reverse=reverse) 
-#sorts according to the posindex provided,since length has to be an int,has to sort differently,reverse = reverse allows both asc and desc in one func instead of 2
-
-def search(musicdata, posname, usersearchreq):
-    return [x for x in musicdata if x[posindex[posname]] == usersearchreq]
+    def sort(self, posname, reverse = False):
+        return sorted(self.musicdata,key=lambda x: int(x[self.posindex[posname]])if posname == "Length" else x[self.posindex[posname]],reverse=reverse)
+#sorts according to the posindex provided,since length has to be an int,has to sort differently,
+#reverse = reverse allows both asc and desc in one func instead of 2
+    def search(self, posname, usersearchreq):
+        return [x for x in self.musicdata if x[self.posindex[posname]] == usersearchreq]
 #prints out data only if matching posindex with inputted index 
+#x refers to each item, and only return value that corresponds with the user search
+    def songoutput(self, songs):
+        for song in songs:
+            SongName, ArtistName, Genre, Length = song
+            mins, secs = self.displayedtime(song)
+            print()#placeholder , consider using f string and format specified 02d
 
+    def displayedtime(self, song):
+        length = int(song[self.posindex["Length"]])
+        return length // 60, length % 60 #floor div for min,modulus for second
 
-def songoutput(y):
-    for x in y:
-        SongName,ArtistName,Genre,Length = x
-        print()#format insert here
+    def playlisttime(self, timelimitminutes: int):
+        timelimit = timelimitminutes * 60 #user dont need to enter in seconds,prefer whole numbers,easier to do if used html,css 
+        total = 0
+        playlist = [] #possibly change this to a append to txt, so easier data manipulation , could add a function to clear playlist if its a text file
+        for song in self.musicdata:
+            length = int(song[self.posindex["Length"]])
+            if total + length <= timelimit:
+                playlist.append(song)
+                total += length #same as total = total + length
+        return playlist
 
-#format of file has to be 3d array?
+    def genresort(self):
+        genre_stored = {}
+        for song in self.musicdata:
+            genre = song[self.posindex["Genre"]]
+            length = int(song[self.posindex["Length"]])
+            if genre not in genre_stored:
+                genre_stored[genre] = []
+            genre_stored[genre].append(length) #can remove this part if genre data is not left blank initially, just set some predetermined ones and remove this?
+
+        for genre, lengths in genre_stored.items():  #.item() to give a pair of values of genre and length [('genre',''),('length','')]
+            average_len = sum(lengths) / len(lengths)
+            mins, secs = average_len // 60, int(average_len % 60 
+            print()#placeholder
 file = "musicdata.txt"
-data1 = data(file)
-#songoutput(sortasc/sortdesc(data1,type))
-#2 seperate func for asc/desc data types?
-
-#convert second into minutes
-def displayedtime(sortinglist):
-    len = int(sortinglist[3])
-    return len//60,len%60
-#floor div for minutes,modulus for seconds
-
-timelimit = int(input("Enter time in minutes"))
-timelimit = timelimit * 60 #need to add check in case of not int?
-
-
-playlist = []
-def playlisttime(musicdata,timelimit):
-    total = 0
-    for song in musicdata:
-        length = int(song[posindex["Length"]])
-        if total + length < timelimit:
-            playlist.append(song)
-            total = total + length#use += instead?? idk
-    return playlist #just set a timielimit as userinput
-
-
-def genresort(musicdata):
-    genre_stored = []
-    for song in musicdata:
-        genre = song[posindex["Genre"]]
-        length = int(song[posindex["Length"]])
-        if genre not in genre_stored:
-            genre_stored[genre] = []
-        genre_stored[genre].append(length) #can remove this part if genre data is not left blank initially, just set some predetermined ones and remove this?
-
-    for genre, lengths in genre_stored.items(): #.item() to give a pair of values of genre and length [('genre',''),('length','')]
-        average_len = sum(lengths) / len(lengths)
-        mins, secs = average_len // 60, int(average_len % 60)
-        print()#placeholder
